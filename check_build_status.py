1fed7f621e024a84451f67ddc034cbb8bcb62d63
Christopher Tyson <ctyson@veracode.com>
2021-08-11 16:23:13 -0400

    Add ability to delete a currently running build

--- check_build_status.py
+++ check_build_status.py
@@ -6,13 +6,16 @@ import datetime as dt
 from dateutil.parser import parse
 from veracode_api_signing.plugin_requests import RequestsAuthPluginVeracodeHMAC
 from veracode_api_py import VeracodeAPI
+api_target = "https://analysiscenter.veracode.com/api/5.0/deletebuild.do"
+headers = {"User-Agent": "Python HMAC Example"}
 
 def main():
 
     parser = argparse.ArgumentParser(
-        description='This script checks to see if an app is currently building a policy scan and returns 0 if not and 1 otherwise. Note: Sandbox is optional')
+        description='This script checks to see if an app is currently building a policy scan and returns 0 if not and 1 otherwise or if the --delete flag is set it will delete that build if you have permissions and return a 0 if the build is deleted and 1 if the build is not deleted. Note: Sandbox is optional')
     parser.add_argument('-a', '--app', help='App name to check',required=True)
     parser.add_argument('-s', '--sandbox', default="", help='Sandbox name to check',required=False)
+    parser.add_argument('--delete',default=False, action='store_true', help='If set to True and a build is running it will be deleted', required=False)
     args = parser.parse_args()
 
     data = VeracodeAPI().get_app_list()
@@ -26,7 +29,23 @@ def main():
            for build in builds:
               if 'policy_updated_date' not in build.attrib: 
                  print ('Current build running is ' + build.attrib['version'])
-                 exit(1)
+                 if(args.delete):
+                    print("Deleting build")
+                    try:
+                       response = requests.get(api_target, auth=RequestsAuthPluginVeracodeHMAC(), headers={"User-Agent": "api.py"}, params={'app_id': app.attrib['app_id']})
+                    except requests.RequestException as e:
+                       print("Error occured")
+                       print(e)
+                       sys.exit(1)                 
+                       exit(1)
+                    if response.ok:
+                       #print(response.status_code)
+                       print("Build deleted")
+                       exit(0)
+                    else:
+                       print(response.status_code)                 
+                       exit(1)
+                 exit(0)
            print ('No currently running scans in app: '+args.app)
            exit(0)
         if (app.attrib["app_name"] == args.app) and (args.sandbox != ""):
@@ -41,7 +60,23 @@ def main():
                     build_info = VeracodeAPI().get_build_info(app.attrib["app_id"], build.attrib["build_id"],sandbox.attrib["sandbox_id"])
                     if 'results_ready="false"' in str(build_info): 
                        print ('Current build running in sandbox '+ sandbox.attrib["sandbox_name"]+' is ' + build.attrib['version'])
-                       exit(1)
+                       if(args.delete):
+                          print("Deleting build")
+                          try:
+                             response = requests.get(api_target, auth=RequestsAuthPluginVeracodeHMAC(), headers={"User-Agent": "api.py"}, params={'app_id': app. attrib['app_id'], 'sandbox_id': sandbox.attrib['sandbox_id']})
+                          except requests.RequestException as e:
+                             print("Error occured")
+                             print(e)
+                             sys.exit(1)                 
+                             exit(1)
+                          if response.ok:
+                             #print(response.status_code)
+                             print("Build deleted")
+                             exit(0)
+                          else:
+                             print(response.status_code)                 
+                             exit(1)
+                    exit(0)
                  print ('No currently running scans in sandbox: '+args.sandbox+' of app: '+args.app)
                  exit(0)
     if (not found) and (args.sandbox == ""):
